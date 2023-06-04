de075bb036e7b4f485f27d4c4a20c0e759433627
nnawar706 <nnawar706@gmail.com>
2023-06-04 13:12:00 +0600

    commit 16

--- app/Http/Controllers/EmployeeSalaryController.php
+++ app/Http/Controllers/EmployeeSalaryController.php
@@ -2,10 +2,50 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\EmployeeSalary;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
 
 class EmployeeSalaryController extends Controller
 {
-    public function create(Request $request)
+    public function index()
     {}
+
+    public function create(Request $request)
+    {
+        $validate = Validator::make($request->all(), [
+            'year_id' => 'required|exists:years,id',
+            'month_id' => 'required|exists:months,id',
+            'employee_id' => ['required','exists:employees,id',
+                Rule::unique('employee_salaries')
+                    ->where(function ($query) use($request) {
+                        return $query->where('year_id', $request->year_id)
+                            ->where('month_id',$request->month_id);
+                    })
+            ],
+            'paid_amount' => 'required|numeric'
+        ],[
+            'employee_id.unique' => 'Salary entry has already been recorded for this employee.'
+        ]);
+
+        if($validate->fails())
+        {
+            return response()->json([
+                'status' => false,
+                'error' => $validate->errors()->all()
+            ], 422);
+        }
+
+        EmployeeSalary::create([
+            'year_id' => $request->year_id,
+            'month_id' => $request->month_id,
+            'employee_id' => $request->employee_id,
+            'paid_amount' => $request->paid_amount
+        ]);
+
+        return response()->json([
+            'status' => true,
+        ], 201);
+    }
 }
